0ac573476c536da1400797ea12eb390073660146
Catherine <codingk8@gmail.com>
2018-12-13 21:15:02 +0100

    Update 41_basic_mathematical_operations.js

--- 41_basic_mathematical_operations.js
+++ 41_basic_mathematical_operations.js
@@ -1,6 +1,18 @@
 /* xxx */
 
 /* Ma solution */
-
+function basicOp(operation, value1, value2) {
+  if (operation === "+") {
+    return value1 + value2;
+  } else if (operation === "-") {
+    return value1 - value2;
+  } else if (operation === "*") {
+    return value1 * value2;
+  } else if (operation === "/") {
+    return value1 / value2
+  } else {
+    return "This operation is not known";
+  }
+}
 
 /* Autres solutions, avec un switch en particulier */
