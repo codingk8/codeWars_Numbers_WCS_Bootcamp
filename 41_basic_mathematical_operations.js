21433a66ccdda561d09e0efa8b03aaca39a903ea
Catherine <codingk8@gmail.com>
2018-12-13 21:18:12 +0100

    Update 41_basic_mathematical_operations.js

--- 41_basic_mathematical_operations.js
+++ 41_basic_mathematical_operations.js
@@ -1,4 +1,4 @@
-/* xxx */
+/* See link & ressources in the MD file */
 
 /* Ma solution */
 function basicOp(operation, value1, value2) {
@@ -16,3 +16,39 @@ function basicOp(operation, value1, value2) {
 }
 
 /* Autres solutions, avec un switch en particulier */
+
+function basicOp(operation, value1, value2) {
+    switch (operation) {
+        case '+':
+            return value1 + value2;
+        case '-':
+            return value1 - value2;
+        case '*':
+            return value1 * value2;
+        case '/':
+            return value1 / value2;
+        default:
+            return 0;
+    }
+}
+
+function basicOp(operation, value1, value2) {
+    switch (operation) {
+        case '+': return value1 + value2;
+        case '-': return value1 - value2;
+        case '*': return value1 * value2;
+        case '/': return value1 / value2;
+        default: return 'Operation must be one of + - * /';
+    }
+}
+
+function basicOp(o, a, b) {
+  return eval(a+o+b);
+}
+
+function basicOp(operation, value1, value2)
+{
+  return eval(value1 + operation + value2);
+}
+
+basicOp = (operation, value1, value2) => eval( `${value1} ${operation} ${value2}` )
