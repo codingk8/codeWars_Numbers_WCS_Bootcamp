623dfbc03bad06e9f1edeee3eeab59ea0dbd3780
Catherine <codingk8@gmail.com>
2019-01-21 14:11:29 +0100

    Update 44_breaking_chocolate_problem.js

--- 44_breaking_chocolate_problem.js
+++ 44_breaking_chocolate_problem.js
@@ -1,1 +1,24 @@
+// ES6
+
+const breakChocolate = (n,m) => ((n * m) - 1) <= 0 ? 0 
+
+// ES5
+
+function breakChocolate(n, m) {
+  if(((n * m) - 1) <= 0) {
+    return 0;
+  } return (n * m) -1;
+}
+
+// Other condition
+
+let breakChocolate = function(n, m) {
+ return (n*m === 0) ? 0 : n * m - 1;
+};
+
+// Other
+
+let breakChocolate = function(n, m) {
+  return Math.max(0, n * m - 1);
+};
 
